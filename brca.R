8cddafb29e505bd4bb7b885baabb5fbed5bc9505
bmagalhaes <60456840+bmagalhaes@users.noreply.github.com>
2020-03-02 16:45:02 -0600

    Update brca.R

--- brca.R
+++ brca.R
@@ -4,11 +4,12 @@ library(margins)
 library(dplyr)
 library(kableExtra)
 library(sjPlot)
+library(mosaic)
+
 
 #library(pander)
 #library(knitr)
 #library(sjmisc)
-#library(mosaic)
 
 
 brca = read.csv(url("https://raw.githubusercontent.com/bmagalhaes/ECO395M-HW2/master/brca.csv"))
@@ -51,6 +52,7 @@ ggplot(data = radiologist_long) +
   theme(plot.title = element_text(hjust = 0.5), panel.grid.major.y = element_blank(), panel.grid.minor.y = element_blank()) 
 
 brca2 = brca
+brca2$radiologist_raw = brca2$radiologist
 brca2$radiologist = str_replace(brca2$radiologist, "radiologist","")
 brca2$age = str_replace(brca2$age, "age","")
 brca2$menopause = str_replace(brca2$menopause, "meno","")
@@ -59,37 +61,86 @@ brca2$density = str_replace(brca2$density, "density","")
 brca_new = dummy.data.frame(brca2, names = c("radiologist", "age","menopause", "density") , sep = ".")
 brca_new = subset(brca_new, select = -c(radiologist.13, age.4049,menopause.postHT, density.1))
 
-
 model_1 = glm(recall ~ . - cancer, data=brca_new, family=binomial)
 ame_11 = summary(margins(model_1, variables = list("radiologist.34", "radiologist.66", "radiologist.89", "radiologist.95")))
-ame_12 = summary(margins(model_1, at = list(history = 0, symptoms = 0, age.5059 = 0, age.6069 = 0,age.70plus = 0,
-                           menopause.pre = 0,menopause.postunknown = 0,menopause.postNoHT = 0,
-                           density.2 = 1), variables = list("radiologist.34", "radiologist.66",
-                                                            "radiologist.89", "radiologist.95")))
 
-model_2 = glm(recall ~ (. - cancer)^2, data=brca_new, family=binomial)
+model_2 = glm(recall ~ (history + symptoms + age.5059 + age.6069 + age.70plus + menopause.postNoHT
+              + menopause.postunknown + menopause.pre + density.2 + density.3 + density.4)*(radiologist.34
+              + radiologist.66 + radiologist.89 + radiologist.95), data=brca_new, family=binomial)
 ame_21 = summary(margins(model_2, variables = list("radiologist.34", "radiologist.66", "radiologist.89", "radiologist.95"), vce = "bootstrap"))
-ame_22 = summary(margins(model_2, at = list(history = 0, symptoms = 0, age.5059 = 0, age.6069 = 0,age.70plus = 0,
-                           menopause.pre = 0,menopause.postunknown = 0,menopause.postNoHT = 0,
-                           density.2 = 1), variables = list("radiologist.34", "radiologist.66",
-                                                            "radiologist.89", "radiologist.95"), vce = "bootstrap"))
 
 ame_11 = select(ame_11,-c(4:7))
-ame_12 = select(ame_12,-c(2:10,13:16))
 ame_21 = select(ame_21,-c(4:7))
-ame_22 = select(ame_22,-c(2:10,13:16))
 
-ame = left_join(ame_11, ame_12, by = "factor")
-ame = left_join(ame, ame_21, by = "factor")
-ame = left_join(ame, ame_22, by = "factor")
-colnames(ame) <- c("","AME","se","AME","se","AME","se","AME","se")
+ame = left_join(ame_11, ame_21, by = "factor")
+colnames(ame) <- c("","AME","se","AME","se")
 
 kable(ame) %>%
   kable_styling("striped") %>%
-  add_header_above(c(" " = 1, "AME" = 2, "AME at x*" = 2, "AME" = 2, "AME at x*" = 2)) %>%
-  add_header_above(c(" ", "Model 1" = 4, "Model 2" = 4))
+  add_header_above(c(" " = 1, "Model 1" = 2, "Model 2" = 2))
 
 ###
+
+n = nrow(brca_new)
+
+boot1 = do(100)*{
+  
+  # re-split into train and test cases
+  n_train = round(0.8*n)  # round to nearest integer
+  n_test = n - n_train
+  train_cases = sample.int(n, n_train, replace=FALSE)
+  test_cases = setdiff(1:n, train_cases)
+  brca_train = brca_new[train_cases,]
+  recall_prob = sum(brca_train$recall == 1)/nrow(brca_train)
+  brca_test = brca_new[test_cases,]
+  brca_test = brca_test[rep(seq_len(nrow(brca_test)), 5),]
+  brca_test =  brca_test %>%
+    mutate(n = row_number()) %>%
+    mutate(radiologist.34 = 0) %>%
+    mutate(radiologist.66 = 0) %>%
+    mutate(radiologist.89 = 0) %>%
+    mutate(radiologist.95 = 0)
+  brca_test =  brca_test %>%
+    mutate(radiologist.34 = 0) %>%
+    mutate(radiologist.66 = 0) %>%
+    mutate(radiologist.89 = 0) %>%
+    mutate(radiologist.95 = 0)
+  brca_test = brca_test %>%
+    mutate(radiologist.34 = ifelse(n >= 198 & 394 >= n, 1, 0)) %>%
+    mutate(radiologist.66 = ifelse(n >= 395 & 591 >= n, 1, 0)) %>%
+    mutate(radiologist.89 = ifelse(n >= 592 & 788 >= n, 1, 0)) %>%
+    mutate(radiologist.95 = ifelse(n >= 789 & 985 >= n, 1, 0))
+  
+  # fit to this training set
+  glm_1 = glm(recall ~ . - cancer, data=brca_train, family=binomial)
+  glm_2 = glm(recall ~ (history + symptoms + age.5059 + age.6069 + age.70plus + menopause.postNoHT
+                        + menopause.postunknown + menopause.pre + density.2 + density.3 + density.4)*(radiologist.34
+                        + radiologist.66 + radiologist.89 + radiologist.95), data=brca_train, family=binomial)
+  
+  # predict on this testing set
+  yhat_test1 = predict(glm_1, brca_test, type="response")
+  yhat_test2 = predict(glm_2, brca_test, type="response")
+
+  brca_test$pred1 = yhat_test1
+  brca_test$pred2 = yhat_test2
+  recalls_1 = brca_test %>%
+    group_by(radiologist_raw)  %>%
+    summarize(recalls = mean(pred1))
+  recalls_2 = brca_test %>%
+    group_by(radiologist_raw)  %>%
+    summarize(recalls = mean(pred2))
+  recalls_t = rbind(recalls_1, recalls_2)
+  recalls = as.data.frame(t(recalls_t))
+  recalls = recalls[-1, ]
+  colnames(recalls) <- c("1_radiologist13", "1_radiologist.34", "1_radiologist.66", 
+                         "1_radiologist.89", "1_radiologist.95", "2_radiologist13",
+                         "2_radiologist.34", "2_radiologist.66", 
+                         "2_radiologist.89", "2_radiologist.95")
+  recalls
+  }
+
+###
+
 prob_cancer = sum(brca$cancer == 1)/nrow(brca)
 
 model_cancer = glm(cancer ~ recall, data=brca, family=binomial)
